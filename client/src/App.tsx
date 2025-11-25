308fb7c08142d017efb84546154d6ed230439893
shoc71 <supersonicwind6@gmail.com>
2025-11-25 07:02:32 -0500

    hotfix-7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css';
 import Home from './components/Home';
-import GitHubLogo from './github-logo.png'
 
 function App() {
   return (
     <div className="">
       <Home />
-       
     </div>
   );
 }
